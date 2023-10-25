fd283555447cd640f43ea6bfd3eaac897ccf094a
Karan <karandayani39@gmail.com>
2023-10-25 16:37:42 +0530

    added header responsiveness and hamburger menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,28 +3,57 @@ import "./header.css";
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
+
+    const [hamClass, setHamClass] = React.useState("ham-menu")
+
+    function toggleMenu() {
+        setHamClass(prevClass => (prevClass === "ham-menu" ? "ham-menu display-flex" : "ham-menu"))
+    }
+
     return (
-        <header>
-            <Link className="site-logo" to="/">SHOE RACK</Link>
-            <div className="left">
+        <>
+            <header>
+                <Link className="site-logo" to="/">SHOE RACK</Link>
+                <div className="left">
+                    <NavLink
+                        className={({isActive}) => isActive ? "active-link" : null}
+                        to="/mens"
+                    >MEN</NavLink>
+                    <NavLink
+                        className={({isActive}) => isActive ? "active-link" : null}
+                        to="/womens"
+                    >WOMEN</NavLink>
+                </div>
                 <NavLink
                     className={({isActive}) => isActive ? "active-link" : null}
+                    id="about"
+                    to="/about"
+                >ABOUT</NavLink>
+                
+                <Link
+                    className="cart"
+                >CART</Link>
+
+                <div className="toggle-button" onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+            </header>
+            <div className={hamClass}>
+                <NavLink
+                    onClick={toggleMenu}
                     to="/mens"
                 >MEN</NavLink>
                 <NavLink
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    onClick={toggleMenu}
                     to="/womens"
                 >WOMEN</NavLink>
-            </div>
-            <div>
                 <NavLink
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    onClick={toggleMenu}
                     to="/about"
                 >ABOUT</NavLink>
-                <NavLink
-                    className="cart"
-                >CART</NavLink>
             </div>
-        </header>
+        </>
     )
 }
