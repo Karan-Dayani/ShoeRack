cd7194453812698a819fb7b43c9a7ddb7bffc7e0
Karan <karandayani39@gmail.com>
2023-10-25 17:54:09 +0530

    made home intro section

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ import {
 } from "react-router-dom";
 
 import Layout from "./Components/Layout";
-import Home from "./pages/Home";
 
+import Home from "./pages/Home";
 import Mens from "./pages/Mens";
 import Womens from "./pages/Womens";
 import About from "./pages/About";
+import Cart from "./pages/Cart";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout />}>
@@ -19,6 +20,7 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="mens" element={<Mens />} />
         <Route path="womens" element={<Womens />} />
         <Route path="about" element={<About />} />
+        <Route path="cart" element={<Cart />} />
     </Route>
 ))
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./home.css"
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
-        <h1>Home page goes here...</h1>
+        <>
+            <div className="intro-img">
+                <div className="intro-info">
+                    <h1 className="intro-title">Love the Planet we walk on</h1>
+                    <p className="intro-text">Bibendum fermentum, aenean donec pretium aliquam blandit <br /> tempor imperdiet arcu arcu ut nunc in dictum mauris at ut.</p>
+                    <div className="intro-btns">
+                        <Link className="btn" to="/mens">SHOP MEN</Link>
+                        <Link className="btn" to="/womens">SHOP WOMEN</Link>
+                    </div>
+                </div>
+            </div>
+            <h1>Hallo Everynian</h1>
+        </>
+        
     )
 }
